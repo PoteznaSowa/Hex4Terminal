3392aa91912162f8f976437bffee50f6f4c30148
MightyFixie <maximusf625@gmail.com>
2020-11-14 13:43:13 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using System.IO;
+
+namespace Hex4Terminal {
+	class Program {
+		public static byte[] FileData;   // Безпосередньо сам файл.
+		public static string FilePath;   // Шлях до файлу.
+		public static bool FileChanged;  // Флаг зміни файлу.
+		public static byte[] ClipBoard;  // Буфер обміну.
+		public static bool InsertMode;   // Режим вставки.
+
+		static int Main(string[] args) {
+			Console.OutputEncoding = Encoding.UTF8;
+			Console.InputEncoding = Encoding.Unicode;
+			Console.Title = "Hex4Terminal";
+
+			Console.Clear();
+			for (int i = 0; i < 23; i++)
+				Console.WriteLine("eeeeeeeee eeeeeeeee eeeeeeeee eeeeeeeee eeeeeeeee eeeeeeeee eeeeeeeee eeeeeeeee ");
+			Console.MoveBufferArea(10, 10, 10, 10, 9, 9);
+			Console.ReadKey();
+			return 0;
+			try {
+				FileData = File.ReadAllBytes(args[0]);
+				FilePath = Path.GetFullPath(args[0]);
+			} catch {
+				FileData = new byte[0];
+				FilePath = "Untitled";
+			}
+			Screen.Initialize();
+
+			return 0;
+		}
+	}
+}
