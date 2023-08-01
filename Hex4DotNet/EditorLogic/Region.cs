ff93a3ea267a6ffc8b7926db0439635284e240b4
PoteznaSowa <64582266+PoteznaSowa@users.noreply.github.com>
2023-08-01 20:10:44 +0300

    Update Region.cs

--- Hex4DotNet/EditorLogic/Region.cs
+++ Hex4DotNet/EditorLogic/Region.cs
@@ -1,15 +1,12 @@
 ﻿namespace Hex4Terminal {
 	abstract class Region {
 		public long Size {
-			// Розмір зміненої ділянки файлу.
 			get; protected set;
 		}
 		public long SizeDelta {
-			// На стільки змінюється розмір файлу після внесення зміни.
 			get; protected set;
 		}
 		public long Position {
-			// Де було змінено файл.
 			get; protected set;
 		}
 	}
